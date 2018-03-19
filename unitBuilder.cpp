1ff67252e6e15b35dc616858b14f8833e5d2948d
umearesn <36409760+umearesn@users.noreply.github.com>
2018-03-19 04:55:42 +0300

    Add files via upload

---
+++ unitBuilder.cpp
@@ -0,0 +1,10 @@
+/*#include "unitBuilder.h"
+
+void unitBuilder::basicBuilder(int price, int absolHP, int movement, unit* newUnit) {
+    newUnit->price = price;
+    newUnit->absolHP = absolHP;
+    newUnit->curHP = absolHP;
+    newUnit->movement = movement;
+}
+*/
+
